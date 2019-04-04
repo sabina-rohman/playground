eda1bc2dc36eaae53158b241fa2e607cec10f147
Sabina Barbhuiya <sabina.barbhuiya@yahoo.com>
2019-04-04 11:09:44 +0100

    javascript,bootstrap 4 and python-oop

---
+++ web/html/rough-work/stalker.js
@@ -0,0 +1,6 @@
+var userFirstName = prompt('What is your first name?');
+var userLastName = prompt("What is your last name?");
+var useAge = prompt("What is your age?");
+var fullName = userFirstName + ' ' + userLastName
+console.log('Your full name is ' + fullName);
+console.log('You are ' + useAge + ' ' + 'years old');
