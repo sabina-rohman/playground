fc3560a41da1693ac476699f4d95c2c2da35356d
Sabina Barbhuiya <sabina.barbhuiya@yahoo.com>
2019-04-04 12:21:29 +0100

    initial files for blackjack + implemented class Card and unittest

---
+++ python/blackjack/main/card.py
@@ -0,0 +1,19 @@
+class Card:
+
+    def __init__(self,number,symbol):
+        self.number = number
+        self.symbol = symbol
+
+    def get_number(self):
+        return self.number
+
+    def get_symbol(self):
+        return self.symbol
+
+    def get_value(self):
+        if self.number in ['K', 'Q', 'J']:
+            return 10
+        elif self.number == 'A':
+            return 1
+        else:
+            return int(self.number)
