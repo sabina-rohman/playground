ce922c456a0591b1078ae9e28ff69273bcafe157
Sabina Barbhuiya <sabina.barbhuiya@yahoo.com>
2019-04-25 09:24:39 +0100

    Todo list in javascript and codingbat practice

--- python/generator_tutorial.py
+++ python/generator_tutorial.py
@@ -1,17 +1,17 @@
 class Movies():
 
     def __init__(self):
-        self.__movies = ['a', 'b', 'c']
-        self.__ratings = [1,2,3]
+        self.movies = ['a', 'b', 'c']
+        self.ratings = [1,2,3]
 
     def get_items(self):
-        for i in self.__ratings:
+        for i in self.ratings:
             yield i
 
 
 if __name__ == '__main__':
     new_realease = Movies()
 
-    for i in new_realease.__movies:
+    for i in new_realease.movies:
         print(i)
 
