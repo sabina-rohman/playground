68db0015f63b9c01dd9f1c5920d91c8da114348d
Sabina Barbhuiya <sabina.barbhuiya@yahoo.com>
2019-05-01 21:02:02 +0100

    ZHI PROBLEMS

---
+++ python/zhi_python_excercise/clas_prob_zhi.py
@@ -0,0 +1,18 @@
+class UpperString:
+    def __init__(self):
+        self.result = ''
+
+    def get_string(self):
+        self.result = input("Please provide a string: ")
+
+    def to_upper(self):
+        return self.result.upper()
+
+if __name__ == '__main__':
+    item = UpperString()
+   # item.get_string()
+    print(item.to_upper())
+
+
+
+
