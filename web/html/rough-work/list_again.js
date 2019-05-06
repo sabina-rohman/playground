30d89af64fdcadac2a2895fec5d13adb2cf0162f
Sabina Barbhuiya <sabina.barbhuiya@yahoo.com>
2019-05-06 10:11:15 +0100

    zhi probs

--- web/html/rough-work/list_again.js
+++ web/html/rough-work/list_again.js
@@ -4,10 +4,17 @@ var input = prompt("What would you like to do?");
 
 while(input !== "quit"){
 	if(input === 'list'){
-		console.log(todos);
+		console.log('*********');
+		todos.forEach(function(todo, i){
+			console.log(i + " : " + todo);
+		});
+		console.log('*********');
 	}else if(input === "new"){
 	var newTodo = prompt("Enter new todo");
 		todos.push(newTodo);
+	}else if(input === "delete"){
+		var index = prompt("Enter the index of todo to delete");
+		todos.splice(index,1);
 	}
 	input = prompt("What would you like to do?");
 }
