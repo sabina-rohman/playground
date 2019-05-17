0908393673e2ff12c752cacf0cf927e676fea0a7
Sabina Barbhuiya <sabina.barbhuiya@yahoo.com>
2019-05-17 11:01:06 +0100

    Score keeper project-Javascript

--- web/html/rough-work/score_keeper.js
+++ web/html/rough-work/score_keeper.js
@@ -1,6 +1,6 @@
 var p1button = document.querySelector("#p1");
 var p2button = document.querySelector("#p2");
-// var resetbutton = document.querySelector("#reset");
+var resetbutton = document.querySelector("#reset");
 var p1Score = 0;
 var p1Display = document.querySelector("#p1Display");
 var p2Score = 0;
@@ -9,12 +9,16 @@ var p2Display = document.querySelector("#p2Display");
 // console.log(p1Display);
 var gameOver = false;
 var winningScore = 5;
+var numInput = document.querySelector("input");
+var p = document.querySelector("p");
+var winningScoreDisplay = document.querySelector("p span");
 
 
 p1button.addEventListener("click",function(){
 	if(!gameOver){
 		p1Score++;
 		if(p1Score === winningScore){
+			p1Display.classList.add("winner");
 			gameOver =true;
 		}
 	}
@@ -25,8 +29,29 @@ p2button.addEventListener("click",function(){
 	if(!gameOver){
 		p2Score++;
 		if(p2Score ===  winningScore){
+			p2Display.classList.add("winner");
 			gameOver = true;
 		}
 	}
 	p2Display.textContent = p2Score;
+});
+
+resetbutton.addEventListener("click",function(){
+		reset();
+});
+
+function reset(){
+		p1Score = 0;
+		p2Score = 0;
+		p1Display.textContent = 0;
+		p2Display.textContent = 0;
+		p1Display.classList.remove("winner");
+		p2Display.classList.remove("winner");
+		gameOver = false;
+}
+
+numInput.addEventListener("change",function(){
+	winningScoreDisplay.textContent = this.value;
+	winningScore = Number(this.value);
+	reset();
 });
