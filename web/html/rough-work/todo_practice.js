7dc93e0b22f91b16a68645df6b0279b955b93475
Sabina Barbhuiya <sabina.barbhuiya@yahoo.com>
2019-06-13 16:17:49 +0100

    paperjs- circle

--- web/html/rough-work/todo_practice.js
+++ web/html/rough-work/todo_practice.js
@@ -2,20 +2,47 @@ $("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
+
+//Click on X to delete todo
 $("ul").on("click", "span", function(event){
-	$(this).parent().fadeOut(1000,function(){
-		$(this).remove();
-	// alert("clicked ona span");
-});
-	event.stopPropagation();
+		$(this).parent().fadeOut(500, function(){
+				$(this).remove();
+		});
+		event.stopPropagation();
 });
 
-$('input[type="text"]').on("keypress",function(event){
-	if(event.which === 13){
-		// li.append(input.val());
-		var inputVal = $(this).val();
-		// clear th input.
-		$(this).val("");
-		$("ul").append("<li><span>X</span> " + inputVal + "</li>")
-	}
+
+
+$("input[type='text']").keypress(function(event){
+		if(event.which === 13){
+			// grabbing new todo text from input
+			var todoText = $(this).val();
+			//val("") acts as setter rather than a getter
+			$(this).val("");
+			//create a new li and add to ul
+			$('ul').append("<li><span><i class='fas fa-trash-alt'></i></span> " + todoText+ "</li>");
+		}
 });
+
+// $('li').click(function(){
+// 	if($(this).css("color") === "rgb(128, 128, 128)"){
+// 		$(this).css({
+// 		color:"black",
+// 		textDecoration: "none"
+// 	});	
+// 	}
+// 	else{
+// 		$(this).css({
+// 		color:"gray",
+// 		textDecoration: "line-through"
+// 	});
+// 	}
+	
+// });
+
+// CSS property to use camel casing like textDecoration instead of text-decoration as we use define an object.we have to use objects syntax.
+
+
+$(".fa-plus").click(function(){
+	$('input[type="text"]').fadeToggle();
+})
