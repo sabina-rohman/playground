a251c9459c88715015e1539495f444e05acce559
Sabina Barbhuiya <sabina.barbhuiya@yahoo.com>
2019-07-02 21:28:27 +0100

    flexbox and bootstrap revision

---
+++ web/html/rough-work/stalker2.js
@@ -0,0 +1,8 @@
+var first = prompt("What is your first name?");
+var last = prompt("What is your last name?");
+var age = prompt("What is your age?");
+var food = prompt("What is your favourite food?");
+
+console.log("Your name is " + first + " " + last);
+console.log("You are " + age +" years old");
+console.log("You love to eat " + food);
