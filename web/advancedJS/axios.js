b8b2badb0a8a002ba172b4ad95e4a60cc74aca0f
sabina-rohman <sabina.barbhuiya@yahoo.com>
2020-02-01 15:37:06 +0000

    Axios for making HTTP requests

---
+++ web/advancedJS/axios.js
@@ -0,0 +1,9 @@
+var url = "https://opentdb.com/api.php?amount=1"
+
+axios.get(url)
+.then(function(res){
+	console.log(res.data.results[0].question);
+})
+.catch(function(){
+	console.log("Error");
+})
