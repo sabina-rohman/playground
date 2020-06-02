62e3ec087f62f08a7a2ff5974cb16cec351a00b4
sabina-rohman <sabina.barbhuiya@yahoo.com>
2020-06-02 14:51:16 +0100

    html page included

--- web/advancedJS/todosApp/index.js
+++ web/advancedJS/todosApp/index.js
@@ -5,13 +5,15 @@ var express = require('express'),
 
 var todoRoutes = require('./routes/todos');
 var notTodoRoutes = require('./routes/notTodos');
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(public));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.get('/', function(req, res){
-    res.send('HELLO FROM ROOT ROUTE!!');
+    res.sendFile('index.html');
 });
 
 app.use('/api/todos/', todoRoutes);
