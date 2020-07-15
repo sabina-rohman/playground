549077d1b3d1a57ab0be2e3589a9af56f7eb1ede
Sabina Barbhuiya <sabina.barbhuiya@yahoo.com>
2020-07-15 15:48:35 +0100

    Resipe-prop-starter css and design

--- web/advancedJS/React/recipe-prop-starter2/src/App.js
+++ web/advancedJS/React/recipe-prop-starter2/src/App.js
@@ -5,7 +5,12 @@ import Recipe from './Recipe';
 function RecipeApp() {
   return (
     <div className="App">
-      <Recipe title="pasta" ingredients={['flour', 'water']} />
+      <Recipe 
+        title="pasta" 
+        ingredients={['flour', 'water']} 
+        instructions="Mix ingredients"
+        img="https://images.unsplash.com/photo-1579684947550-22e945225d9a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80"
+        />
     </div>
   );
 }
--- web/advancedJS/React/recipe-prop-starter2/src/Recipe.js
+++ web/advancedJS/React/recipe-prop-starter2/src/Recipe.js
@@ -1,19 +1,28 @@
 import React from 'react';
+import './Recipe.css';
 
 function Recipe(props) {
-    const {title} = props;
+    const {title, img, instructions} = props;
     // short way of saying => const title = props.title;
     const ingredients = props.ingredients.map((ing, index) => (
         <li key={index}>{ing}</li>
     ))
-return ( 
-    <div>
-        <div> Recipe {title}</div>
-        <ul>
-            {ingredients}
-        </ul>
-    </div>
-    );
+    return ( 
+        <div className="recipe-card">
+            <div className="recipe-card-image">
+                <img src={img} alt={title} />
+            </div>
+            <div className="recipe-card-content">
+                <h3 className="recipe-title"> Recipe {title}</h3>
+                <h4>Ingredients:</h4>
+                <ul>
+                    {ingredients}
+                </ul>
+                <h4>Instructions:</h4>
+                <p>{instructions}</p>
+            </div>
+        </div>
+     );
 }
 
 export default Recipe;
