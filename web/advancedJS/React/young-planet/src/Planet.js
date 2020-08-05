7aae9b1884fa64dafdaf0ee34e3d271ca396efe2
Sabina Barbhuiya <sabina.barbhuiya@yahoo.com>
2020-08-05 15:48:06 +0100

    changes in grid system

--- web/advancedJS/React/young-planet/src/Planet.js
+++ web/advancedJS/React/young-planet/src/Planet.js
@@ -7,14 +7,18 @@ function Planet(props){
     <div className="planet" >
         <p>{props.data.name}</p>
         <p>{props.data.earthYears}</p>
-        <p>Age in this planet:{props.planetAge(props.data.earthYears, 32)}</p>
+        <p>{props.age}</p>
+        <p>{props.weight}</p>
+        <p>Age in this planet:{props.planetAge(props.data.earthYears, props.age)}</p>
+        <p>Weight in this planet:{props.planetWeight(props.data.earthWeight, props.weight)}</p>
     </div>
     )
 }
 
 
 Planet.defaultProps = {
-    planetAge: (earthYears, ageInYears) => Number((ageInYears/earthYears).toFixed(2))
+    planetAge: (earthYears, ageInYears) => Number((ageInYears/earthYears).toFixed(2)),
+    planetWeight: (earthWeight, weightInKg) => Number((earthWeight*weightInKg).toFixed(2))
 }
 
 // Planet.propTypes = {
--- web/advancedJS/React/young-planet/src/PlanetList.js
+++ web/advancedJS/React/young-planet/src/PlanetList.js
@@ -5,41 +5,58 @@ import Planet from './Planet';
 class PlanetList extends Component {
     constructor(props){
         super(props);
-
-        this.state = {fontSize: '1.5em'}
-
-        // setInterval(() => {
-        //     const color = 'white'? 'black' : 'white'
-        //     const fontSize = '1.5em'? '3em' : '1.5em'
-        //     this.setState({fontSize: fontSize, color: color})
-        // }, 300)
+        this.state = {age: null, weight: 0}
     }
+
     render(){
         const planetNames = this.props.planets.map((planet, index) => (
             <Planet 
                 key={index} 
-                data={planet} 
+                data={planet}
+                age={this.state.age}
+                weight={this.state.weight}
             />
         ));
         return(
-            <div style={{backgroundColor: 'black', fontSize: this.state.fontSize}}>
+            <div>
+                <input 
+                    placeholder="Enter your Age"
+                    type="text"
+                    className="newAge"
+                    onChange={(e) => this.setState({age: this.props.getNumber(e.target.value)})}
+                />
+                <input 
+                    placeholder="Enter your weight"
+                    type="number"
+                    onChange={(e) =>this.setState({weight: e.target.value})}
+                />
+                <div>Age:-{this.state.age}</div>
+                <div>Weight:-{this.state.weight}</div>
                 <ul>
-                   {planetNames} 
+                   {planetNames}
                 </ul>
             </div>
         )
     }
 }
 
+
 PlanetList.defaultProps = {
-    planets : [{name: 'mercury', earthYears: 0.2408467},
-                {name: 'venus', earthYears: 0.61519726}, 
-                {name: 'earth', earthYears: 1.0}, 
-                {name: 'mars', earthYears: 1.8808158}, 
-                {name: 'jupiter', earthYears: 11.862615}, 
-                {name: 'saturn', earthYears: 29.447498}, 
-                {name: 'uranus', earthYears: 84.016846}, 
-                {name: 'neptune', earthYears: 164.79132}]
+    planets : [{name: 'mercury', earthYears: 0.2408467, earthWeight: 0.378},
+                {name: 'venus', earthYears: 0.61519726, earthWeight: 0.907}, 
+                {name: 'earth', earthYears: 1.0, earthWeight: 1}, 
+                {name: 'mars', earthYears: 1.8808158, earthWeight: 0.377}, 
+                {name: 'jupiter', earthYears: 11.862615, earthWeight: 0.2528}, 
+                {name: 'saturn', earthYears: 29.447498, earthWeight: 0.1064}, 
+                {name: 'uranus', earthYears: 84.016846, earthWeight: 0.889}, 
+                {name: 'neptune', earthYears: 164.79132, earthWeight: 0.1125}],
+    getNumber: (s) => {
+        if(s.length > 0){
+            return Number(s)
+        } else {
+            return 0
+        }
+    }
 }
 
 
