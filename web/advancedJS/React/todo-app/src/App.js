3664d468384d4e483d6194ac23bbe68278fec4eb
Sabina Barbhuiya <sabina.barbhuiya@yahoo.com>
2020-08-05 17:15:44 +0100

    wrote todo item

--- web/advancedJS/React/todo-app/src/App.js
+++ web/advancedJS/React/todo-app/src/App.js
@@ -1,6 +1,59 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// const TodoItem = ({text}) => (
+//   <li>{text}</li>
+// )
+
+// class App extends Component {
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//       todos: [],
+//       newTodo: ''
+//     }
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
+
+//   handleSubmit(e) {
+//     e.preventDefault();
+//     const todos = [...this.state.todos, this.state.newTodo];
+//     this.setState({todos, newTodo: ''});
+//   }
+
+//   render(){
+//     const {newTodo} = this.state;
+//     const todos = this.state.todos.map((t, i) => (
+//       <TodoItem key={i} text={t} />
+//     ));
+//     return(
+//       <div className="App">
+//         <h1>Simple Todo App</h1>
+//         <form onSubmit={this.handleSubmit} >
+//             <input 
+//                 className="todo-input"
+//                 type="text"
+//                 placeholder="Put todos here"
+//                 name="newTodo"
+//                 value= {newTodo}
+//                 onChange={(e) => this.setState({[e.target.name]: e.target.value})}
+//               />
+//             <button 
+//               className="save-button"
+//               type="submit" 
+//               style={{marginLeft: '20px'}}>
+//               Save
+//             </button>
+//         </form>
+
+//         <div className="todo-content">
+//         <ol>{todos}</ol>
+//         </div>
+
+//       </div>
+//     );
+//   }
+// }
 const TodoItem = ({text}) => (
   <li>{text}</li>
 )
@@ -12,7 +65,8 @@ class App extends Component {
       todos: [],
       newTodo: ''
     }
-    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   handleSubmit(e) {
@@ -29,29 +83,27 @@ class App extends Component {
     return(
       <div className="App">
         <h1>Simple Todo App</h1>
-        <form onSubmit={this.handleSubmit} >
-            <input 
-                className="todo-input"
-                type="text"
-                placeholder="Put todos here"
-                name="newTodo"
-                value= {newTodo}
-                onChange={(e) => this.setState({[e.target.name]: e.target.value})}
-              />
+        <form onSubmit={this.handleSubmit}>
+          <input 
+            className="todo-input"
+            type="text"
+            placeholder="Enter the todo here"
+            name="newTodo"
+            value={newTodo}
+            onChange={(e) => this.setState({newTodo: e.target.value})}
+          />
             <button 
-              className="save-button"
-              type="submit" 
-              style={{marginLeft: '20px'}}>
-              Save
-            </button>
+            type="submit"
+            className="save-button"
+            >
+            Save
+          </button>
         </form>
-
         <div className="todo-content">
-        <ol>{todos}</ol>
+          <ol>{todos}</ol>
         </div>
-
-      </div>
-    );
+    </div>
+    )
   }
 }
 
